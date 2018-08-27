b210c3b919d85d819fe5229e82e2ffc55cf9727f
denyszamiatin <denys.zamiatin@gmail.com>
2018-08-27 16:57:14 +0300

    Functions

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 todo = {}
 
+
+def _append_task(date, task):
+    if date not in todo:
+        todo[date] = []
+    todo[date].append(task)
+
+
+def _list_tasks(date):
+    if date not in todo:
+        raise ValueError
+    return tuple(todo[date])
+
+
+def append_task():
+    date = input('Date?')
+    task = input('Task?')
+    _append_task(date, task)
+
+
+def list_tasks():
+    date = input('Date?')
+    try:
+        for task in _list_tasks(date):
+            print(task)
+    except ValueError:
+        print('There are no tasks on this day')
+
+
 while True:
     print("""a - append task
 l - list tasks
 q - quit""")
     action = input('?').lower()
     if action == 'a':
-        date = input('Date?')
-        task = input('Task?')
-        if date not in todo:
-            todo[date] = []
-        todo[date].append(task)
+        append_task()
     elif action == 'l':
-        date = input('Date?')
-        if date not in todo:
-            print('There are no tasks on this day')
-        else:
-            print(date)
-            for task in todo[date]:
-                print(task)
+        list_tasks()
     elif action == 'q':
         break
